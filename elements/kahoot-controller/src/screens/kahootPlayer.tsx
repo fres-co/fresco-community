2e90109582f8963d8a9a64af6105f132dc699aaf
Maxence Dalmais <maxence@fres.co>
2023-01-20 11:53:26 +0100

    :sparkles: allow to set pin for other participants

--- elements/kahoot-controller/src/screens/kahootPlayer.tsx
+++ elements/kahoot-controller/src/screens/kahootPlayer.tsx
@@ -2,14 +2,14 @@ const KahootURL = 'https://kahoot.it'
 
 import { Styled } from '../components/Styled'
 
-export const KahootPlayer = ({ pin } : { pin?: number }) => {
+export const KahootPlayer = ({ pin, canSetPin, resetPin } : { pin?: number, canSetPin: boolean, resetPin: () => void }) => {
 
     const url = new URL(KahootURL)
     if (pin) {
         url.searchParams.set('pin', pin)
     }
     
-    return <Styled
+    return <><Styled
         el={<iframe src={url.toString()} />}
         css={`
             width: 100%;
@@ -17,4 +17,11 @@ export const KahootPlayer = ({ pin } : { pin?: number }) => {
             border: none;
         }`
     } />
+        {canSetPin && <Styled
+            css={`position: absolute; top: 10px; left:10px; padding: 10px; border-radius: 30px; font-weight: bold; cursor: pointer; background: white`}
+            onClick={resetPin}>
+            RESET PIN
+            </Styled>}
+    
+    </>
 }
