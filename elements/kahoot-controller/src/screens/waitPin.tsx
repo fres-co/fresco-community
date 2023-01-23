24e9709b83361b6719df1bdb157a65293f0d065f
Maxence Dalmais <maxence@fres.co>
2023-01-23 09:18:06 +0100

    :sparkles: add wait pin

---
+++ elements/kahoot-controller/src/screens/waitPin.tsx
@@ -0,0 +1,13 @@
+import { Styled } from "../components/Styled";
+
+export const WaitPin = () => <Styled
+    css='
+    background-color: red;
+    height: 100%;
+    text-align: center;
+    color: white;
+    font-size: 24px;
+    padding: 2rem;
+    background-color: rgb(56, 18, 114);
+    '
+>Waiting for the admin to set Pin</Styled>
